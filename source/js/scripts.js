51b51138ea5b81f37398424fca644b0f3d094cc5
Lucas <ll@lucaslemonnier.com>
2018-01-07 12:19:13 -0500

    Add content and markup

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,4 +1,23 @@
-/*
- * Add all your javascripts here!
- * Don't use js in this project? No problem, just delete the js directory
-*/
+// Get next month
+function nextMonth() {
+  var month = new Array();
+  month[0] = "January";
+  month[1] = "February";
+  month[2] = "March";
+  month[3] = "April";
+  month[4] = "May";
+  month[5] = "June";
+  month[6] = "July";
+  month[7] = "August";
+  month[8] = "September";
+  month[9] = "October";
+  month[10] = "November";
+  month[11] = "December";
+
+  var d = new Date();
+  var n = month[d.getMonth() + 1];
+  return n;
+}
+
+// Run next month on .month span
+document.querySelector(".month").innerHTML = nextMonth();
